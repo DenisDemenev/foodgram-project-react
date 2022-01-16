36273a89c61db9f87ce7e722aa7688d1f7db6a43
DenisDemenev <lazer89@list.ru>
2022-01-16 18:29:39 +0300

    revie 2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -42,9 +42,8 @@ class CustomUserSerializer(UserSerializer):
 
     def get_is_subscribed(self, obj):
         user = self.context.get('request').user
-        if user.is_anonymous:
-            return False
-        return Follow.objects.filter(user=user, author=obj.id).exists()
+        return (not user.is_anonymous
+                and Follow.objects.filter(user=user, author=obj.id).exists())
 
 
 class IngredientSerializer(serializers.ModelSerializer):
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -4,11 +4,13 @@ from recipes.models import (Cart, Favorite, Ingredient, Recipe,
                             RecipeIngredient, Tag)
 
 
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'color', 'slug')
     empty_value_display = "-пусто-"
 
 
+@admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     list_filter = ('name',)
@@ -21,6 +23,7 @@ class RecipeIngredientInline(admin.TabularInline):
     extra = 1
 
 
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     inlines = (RecipeIngredientInline,)
     list_display = ('author', 'name', 'favorite_count')
@@ -31,24 +34,19 @@ class RecipeAdmin(admin.ModelAdmin):
         return Favorite.objects.filter(recipe=obj).count()
 
 
+@admin.register(RecipeIngredient)
 class RecipeIngredientAdmin(admin.ModelAdmin):
     list_display = ('id', 'recipe', 'ingredient', 'amount')
     empty_value_display = "-пусто-"
 
 
+@admin.register(Favorite)
 class FavoriteRecipeAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'recipe')
     empty_value_display = "-пусто-"
 
 
+@admin.register(Cart)
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'recipe')
     empty_value_display = "-пусто-"
-
-
-admin.site.register(Tag, TagAdmin)
-admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
-admin.site.register(Favorite, FavoriteRecipeAdmin)
-admin.site.register(Cart, ShoppingCartAdmin)
